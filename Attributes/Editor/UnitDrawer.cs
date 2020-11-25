11f6b0628cbeeac7c352f7a1dd5bcef7ae55006d
Unknown <nkmacadam@gmail.com>
2020-11-25 12:34:21 -0500

    Added attributes

---
+++ Attributes/Editor/UnitDrawer.cs
@@ -0,0 +1,33 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Oni.Attributes.Editor
+{
+	/// <summary>
+	/// Drawer for drawing unit attribute
+	/// </summary>
+	[CustomPropertyDrawer(typeof(UnitAttribute), true)]
+	public class UnitDrawer : PropertyDrawer
+	{
+		private const int _rightPadding = 2;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			UnitAttribute unit = attribute as UnitAttribute;
+
+			Rect unitPosition = new Rect(position.x, position.y, position.width - _rightPadding, position.height);
+
+			GUIStyle unitStyle = new GUIStyle(EditorStyles.label);
+			unitStyle.alignment = TextAnchor.MiddleRight;
+
+			EditorGUI.PropertyField(position, property);
+
+			GUI.enabled = false;
+			EditorGUI.LabelField(unitPosition, unit.unitSymbol, unitStyle);
+			GUI.enabled = true;
+		}
+	}
+}
---
+++ Attributes/UnitAttribute.cs
@@ -0,0 +1,20 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+
+namespace Oni.Attributes
+{
+	/// <summary>
+	/// Adds a unit suffix to the value in the inspector (ex. cm, ft, %)
+	/// </summary>
+	public class UnitAttribute : PropertyAttribute
+	{
+		public string unitSymbol;
+
+		public UnitAttribute(string unitSymbol)
+		{
+			this.unitSymbol = unitSymbol;
+		}
+	}
+}

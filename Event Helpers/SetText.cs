85f5179c80d362da693413f3ea686135b9625aae
Unknown <nkmacadam@gmail.com>
2020-11-24 20:01:05 -0500

    Added event helper scripts

---
+++ Event Helpers/SetText.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DarumaKit.EventHelpers
+{
+    /// <summary>
+    /// Provides helper methods to set UI Text with a UnityEvent
+    /// </summary>
+    public class SetText : MonoBehaviour
+    {
+        [SerializeField] private Text _text = default;
+
+        public Text Text { get => _text; set => _text = value; }
+
+        public void Set<T>(T value)
+        {
+            _text.text = value.ToString();
+        }
+
+        public void Set(int value)
+        {
+            Set<int>(value);
+        }
+
+        public void Set(float value)
+        {
+            Set<float>(value);
+        }
+
+        public void Set(Vector2 value)
+        {
+            Set<Vector2>(value);
+        }
+
+        public void Set(Vector3 value)
+        {
+            Set<Vector3>(value);
+        }
+    }
+}
+
---
+++ Event Helpers/TMP_SetText.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace DarumaKit.EventHelpers
+{
+    /// <summary>
+    /// Provides helper methods to set Text Mesh Pro Text with a UnityEvent
+    /// </summary>
+    public class TMP_SetText : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text = default;
+
+        public TextMeshProUGUI Text { get => _text; set => _text = value; }
+
+        public void Set<T>(T value)
+        {
+            _text.text = value.ToString();
+        }
+
+        public void Set(int value)
+        {
+            Set<int>(value);
+        }
+
+        public void Set(float value)
+        {
+            Set<float>(value);
+        }
+
+        public void Set(Vector2 value)
+        {
+            Set<Vector2>(value);
+        }
+
+        public void Set(Vector3 value)
+        {
+            Set<Vector3>(value);
+        }
+    }
+}
+

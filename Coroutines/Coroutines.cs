ca560e0b76ce5caee61f8ead97e50e3ac21052b2
Unknown <nkmacadam@gmail.com>
2020-11-24 19:48:50 -0500

    Added coroutines scripts

---
+++ Coroutines/Coroutines.cs
@@ -0,0 +1,56 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using System.Collections;
+using UnityEngine;
+
+namespace Oni
+{
+    public static class Coroutines
+    {
+        public static IEnumerator ScheduleAction(float delay, System.Action action, bool realTime = false)
+        {
+            if (!realTime) yield return new WaitForSeconds(delay);
+            else yield return new WaitForSecondsRealtime(delay);
+            action.Invoke();
+        }
+    }
+
+    public class WaitUntilOrTimeOut : CustomYieldInstruction
+    {
+        private System.Func<bool> _predicate;
+        private float _timeOutTime;
+
+        public override bool keepWaiting => Time.realtimeSinceStartup < _timeOutTime && !_predicate();
+
+        /// <summary>
+        /// Wait until predicate is true or the yield instruction times out
+        /// </summary>
+        /// <param name="predicate">The predicate to evaluate</param>
+        /// <param name="timeOutAfter">The duration before the yield instruction times out and continues</param>
+        public WaitUntilOrTimeOut(System.Func<bool> predicate, float timeOutAfter)
+        {
+            _predicate = predicate;
+            _timeOutTime = Time.realtimeSinceStartup + timeOutAfter;
+        }
+    }
+
+    public class WaitWhileOrTimeOut : CustomYieldInstruction
+    {
+        private System.Func<bool> _predicate;
+        private float _timeOutTime;
+
+        public override bool keepWaiting => Time.realtimeSinceStartup < _timeOutTime && _predicate();
+
+        /// <summary>
+        /// Wait while predicate is false or until the yield instruction times out
+        /// </summary>
+        /// <param name="predicate">The predicate to evaluate</param>
+        /// <param name="timeOutAfter">The duration before the yield instruction times out and continues</param>
+        public WaitWhileOrTimeOut(System.Func<bool> predicate, float timeOutAfter)
+        {
+            _predicate = predicate;
+            _timeOutTime = Time.realtimeSinceStartup + timeOutAfter;
+        }
+    }
+}

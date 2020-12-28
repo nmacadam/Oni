8d9902bd8f7f6cd0eb4d7606d1345c995870b1cf
Unknown <nkmacadam@gmail.com>
2020-12-28 14:15:32 -0600

    Added wait coroutines with timeout and assert

---
+++ Test Utilities/AssertionCoroutines.cs
@@ -0,0 +1,74 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Oni.TestUtilities
+{
+    public class WaitWhileOrAssertFailAfter : CustomYieldInstruction
+    {
+        private System.Func<bool> _predicate;
+        private float _timeOutTime;
+
+		public override bool keepWaiting
+        {
+            get
+            {
+                bool valid = Time.realtimeSinceStartup < _timeOutTime && _predicate(); 
+
+                if (!valid)
+                {
+                    Assert.Fail($"Coroutine timed out and failed after {_timeOutTime}s");
+                }
+
+                return valid;
+            }
+            
+        }
+
+        /// <summary>
+        /// Wait while predicate is false or until the yield instruction times out and fails
+        /// </summary>
+        /// <param name="predicate">The predicate to evaluate</param>
+        /// <param name="timeOutAfter">The duration before the yield instruction times out and fails</param>
+        public WaitWhileOrAssertFailAfter(System.Func<bool> predicate, float timeOutAfter)
+        {
+            _predicate = predicate;
+            _timeOutTime = Time.realtimeSinceStartup + timeOutAfter;
+        }
+    }
+
+    public class WaitUntilOrAssertFailAfter : CustomYieldInstruction
+    {
+        private System.Func<bool> _predicate;
+        private float _timeOutTime;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                bool valid = Time.realtimeSinceStartup < _timeOutTime && !_predicate(); 
+
+                if (!valid)
+                {
+                    Assert.Fail($"Coroutine timed out and failed after {_timeOutTime}s");
+                }
+
+                return valid;
+            }
+            
+        }
+
+        /// <summary>
+        /// Wait until predicate is true or the yield instruction times out and fails
+        /// </summary>
+        /// <param name="predicate">The predicate to evaluate</param>
+        /// <param name="timeOutAfter">The duration before the yield instruction times out and fails</param>
+        public WaitUntilOrAssertFailAfter(System.Func<bool> predicate, float timeOutAfter)
+        {
+            _predicate = predicate;
+            _timeOutTime = Time.realtimeSinceStartup + timeOutAfter;
+        }
+    }
+}

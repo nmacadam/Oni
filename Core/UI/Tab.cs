e94b719d570240af7b0158cab0d18214f87753a1
Unknown <nkmacadam@gmail.com>
2020-12-28 19:04:49 -0600

    Added Tab and TabGroup

---
+++ Core/UI/Tab.cs
@@ -0,0 +1,58 @@
+﻿// Original work Copyright (c) 2019 Game Dev Guide
+// CC-BY License
+// https://www.youtube.com/watch?v=211t6r12XPQ
+
+// Modified for ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Oni.UI
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Tab : Selectable
+	{
+		[SerializeField] private TabGroup _tabGroup = default;
+		[SerializeField] private UnityEvent _onTabSelected = default;
+		[SerializeField] private UnityEvent _onTabDeselected = default;
+
+        public TabGroup TabGroup { get => _tabGroup; set => _tabGroup = value; }
+        public UnityEvent OnTabSelected { get => _onTabSelected; set => _onTabSelected = value; }
+        public UnityEvent OnTabDeselected { get => _onTabDeselected; set => _onTabDeselected = value; }
+
+        public override void OnSelect(BaseEventData eventData)
+        {
+			base.OnSelect(eventData);
+            if(_tabGroup != null) _tabGroup.OnTabSelected(this);
+			else Select();
+        }
+
+		public override void OnDeselect(BaseEventData eventData)
+		{
+			base.OnDeselect(eventData);
+            if(_tabGroup != null) _tabGroup.OnTabExit(this);
+			else Deselect();
+		}
+
+		public override void Select()
+		{
+			_onTabSelected.Invoke();
+		}
+
+		public void Deselect()
+		{
+			_onTabDeselected.Invoke();
+		}
+
+        protected override void Awake()
+		{
+			base.Awake();
+			if(_tabGroup != null) _tabGroup.Subscribe(this);
+		}
+	}
+}
---
+++ Core/UI/TabGroup.cs
@@ -0,0 +1,89 @@
+﻿// Original work Copyright (c) 2019 Game Dev Guide
+// CC-BY License
+// https://www.youtube.com/watch?v=211t6r12XPQ
+
+// Modified for ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Oni.UI
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class TabGroup : UIBehaviour
+	{
+		[SerializeField] private Tab _selectedTab;
+
+		private List<Tab> tabs = new List<Tab>();
+
+		public void Subscribe(Tab tab)
+		{
+			tabs.Add(tab);
+		}
+
+		public void OnTabEnter(Tab tab)
+		{
+			ResetTabs();
+			if (_selectedTab == null || tab != _selectedTab)
+			{
+				tab.targetGraphic.color = tab.colors.highlightedColor;
+			}
+		}
+
+		public void OnTabExit(Tab tab)
+		{
+			ResetTabs();
+		}
+
+		public void OnTabSelected(Tab tab)
+		{
+			if (_selectedTab != null)
+			{
+				_selectedTab.Deselect();
+			}
+
+			_selectedTab = tab;
+			_selectedTab.Select();
+
+			ResetTabs();
+			tab.targetGraphic.color = tab.colors.selectedColor;
+		}
+
+		public void ResetTabs()
+		{
+			foreach (var tab in tabs)
+			{
+				if (_selectedTab != null && tab == _selectedTab)
+				{
+					continue;
+				}
+
+				tab.targetGraphic.color = tab.colors.normalColor;
+			}
+		}
+
+		public void ClearSelection()
+		{
+			foreach (var tab in tabs)
+			{
+				if (_selectedTab != null && tab == _selectedTab)
+				{
+					tab.Deselect();
+				}
+				tab.targetGraphic.color = tab.colors.normalColor;
+			}
+		}
+
+		private void Start()
+		{
+			if (_selectedTab != null)
+			{
+				_selectedTab.targetGraphic.color = _selectedTab.colors.selectedColor;
+			}
+		}
+	}
+}

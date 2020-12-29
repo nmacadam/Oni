10c73d2a2a62d58533972c3027f37a297bbcc149
Unknown <nkmacadam@gmail.com>
2020-12-28 18:36:15 -0600

    Added extensions

---
+++ Core/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,35 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+
+namespace Oni
+{
+	public static class GameObjectExtensions
+	{
+		/// <summary>
+		/// Gets a component from a GameObject if present, or adds one if missing
+		/// </summary>
+		public static T GetOrAddComponent<T>(this GameObject gameObject) where T : Component
+		{
+			var component = gameObject.GetComponent<T>();
+
+			if (component != null)
+			{
+				return component;
+			}
+			else
+			{
+				return gameObject.AddComponent<T>();
+			}
+		}
+
+		/// <summary>
+		/// Checks whether the GameObject has a component of the given type
+		/// </summary>
+		public static bool HasComponent<T>(this GameObject gameObject) where T : Component
+		{
+			return gameObject.GetComponent<T>() != null;
+		}
+	}
+}
---
+++ Core/Extensions/TransformExtensions.cs
@@ -0,0 +1,48 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+
+namespace Oni
+{
+	public static class TransformExtensions
+	{
+		/// <summary>
+		/// Adds a list of GameObjects as children of this transform
+		/// </summary>
+		/// <param name="transform">This</param>
+		/// <param name="children">List of GameObjects to child</param>
+		public static void AddChildren(this Transform transform, GameObject[] children)
+		{
+			foreach (var child in children)
+			{
+				child.transform.parent = transform;
+			}
+		}
+		
+		/// <summary>
+		/// Adds a list of Transforms as children of this transform
+		/// </summary>
+		/// <param name="transform">This</param>
+		/// <param name="children">List of Transforms to child</param>
+		public static void AddChildren(this Transform transform, Transform[] children)
+		{
+			foreach (var child in children)
+			{
+				child.parent = transform;
+			}
+		}
+
+		/// <summary>
+		/// Matches the elements of this transform to another
+		/// </summary>
+		/// <param name="transform">This transform</param>
+		/// <param name="other">Transform to match</param>
+		public static void MatchTransform(this Transform transform, Transform other)
+		{
+			transform.position = other.position;
+			transform.rotation = other.rotation;
+			transform.localScale = other.localScale;
+		}
+	}
+}

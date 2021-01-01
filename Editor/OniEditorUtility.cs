0e690fb3fc9fecc78c7c088ad7b7efb14d8ba317
Unknown <nkmacadam@gmail.com>
2020-12-31 22:12:09 -0600

    Added Editor utilities

---
+++ Editor/OniEditorUtility.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) 2015, Dylan Engelman, Jupiter Lighthouse Studio
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// https://github.com/lordofduct/spacepuppy-unity-framework-3.0
+// Original Content as follows:
+// - GetTargetObjectOfProperty
+// - GetValue_Imp
+// 
+// Modified for ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+// Remaining content is part of ONI, 
+// Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Oni.Editor
+{
+	public static class OniEditorUtility
+	{
+		/// <summary>
+        /// Gets the object the property represents.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static object GetTargetObjectOfProperty(SerializedProperty prop)
+        {
+            if (prop == null) return null;
+
+            var path = prop.propertyPath.Replace(".Array.data[", "[");
+            object obj = prop.serializedObject.targetObject;
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = System.Convert.ToInt32(element.Substring(element.IndexOf("[")).Replace("[", "").Replace("]", ""));
+                    obj = GetValue_Imp(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue_Imp(obj, element);
+                }
+            }
+            return obj;
+        }
+
+        private static object GetValue_Imp(object source, string name)
+        {
+            if (source == null)
+                return null;
+            var type = source.GetType();
+
+            while (type != null)
+            {
+                var f = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                if (f != null)
+                    return f.GetValue(source);
+
+                var p = type.GetProperty(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                if (p != null)
+                    return p.GetValue(source, null);
+
+                type = type.BaseType;
+            }
+            return null;
+        }
+
+        private static object GetValue_Imp(object source, string name, int index)
+        {
+            var enumerable = GetValue_Imp(source, name) as System.Collections.IEnumerable;
+            if (enumerable == null) return null;
+            var enm = enumerable.GetEnumerator();
+
+            for (int i = 0; i <= index; i++)
+            {
+                if (!enm.MoveNext()) return null;
+            }
+            return enm.Current;
+        }
+	}
+}
---
+++ Editor/OniGUI.cs
@@ -0,0 +1,57 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Oni.Editor
+{
+	/// <summary>
+	/// More GUI functions for the Unity Editor
+	/// </summary>
+	public static class OniGUI
+	{
+		/// <summary>
+		/// Draws a property with multiple property fields for sub-properties
+		/// </summary>
+		/// <param name="label">The main property label</param>
+		/// <param name="subLabels">The sub-property labels</param>
+		/// <param name="properties">The serialized sub-properties</param>
+		public static void DrawMultiFieldProperty(Rect pos, GUIContent label, GUIContent[] subLabels, SerializedProperty[] properties)
+		{
+			float subLabelSpacing = 4f;
+			var contentRect = EditorGUI.PrefixLabel(pos, GUIUtility.GetControlID(FocusType.Passive), label);
+			DrawMultiplePropertyFields(contentRect, subLabels, properties, subLabelSpacing);
+		}
+
+		/// <summary>
+		/// Draws multiple property fields within one rect
+		/// </summary>
+		/// <param name="pos">Rect to draw in</param>
+		/// <param name="subLabels">Labels for content</param>
+		/// <param name="props">Content serialized properties</param>
+		/// <param name="subLabelSpacing">Spacing between content</param>
+		public static void DrawMultiplePropertyFields(Rect pos, GUIContent[] subLabels, SerializedProperty[] props, float subLabelSpacing = 4f) 
+		{
+			// backup gui settings
+			var indent     = EditorGUI.indentLevel;
+			var labelWidth = EditorGUIUtility.labelWidth;
+		
+			// draw properties
+			var propsCount = props.Length;
+			var width      = (pos.width - (propsCount - 1) * subLabelSpacing) / propsCount;
+			var contentPos = new Rect(pos.x, pos.y, width, pos.height);
+			EditorGUI.indentLevel = 0;
+			for (var i = 0; i < propsCount; i++) 
+			{
+				EditorGUIUtility.labelWidth = EditorStyles.label.CalcSize(subLabels[i]).x;
+				EditorGUI.PropertyField(contentPos, props[i], subLabels[i]);
+				contentPos.x += width + subLabelSpacing;
+			}
+	
+			// restore gui settings
+			EditorGUIUtility.labelWidth = labelWidth;
+			EditorGUI.indentLevel       = indent;
+		}
+	}
+}

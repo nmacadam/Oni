54885eebac6074aef91f6d099ece5615dac41177
Unknown <nkmacadam@gmail.com>
2021-01-03 21:06:31 -0600

    Added DisableEditing attribute

---
+++ Core/Attributes/DisableEditingAttribute.cs
@@ -0,0 +1,15 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using System;
+using UnityEngine;
+
+namespace Oni.Attributes
+{
+	/// <summary>
+	/// Disables editing the field in the inspector but still displays it
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class DisableEditingAttribute : PropertyAttribute
+	{ }
+}
---
+++ Editor/Attributes/DisableEditingDrawer.cs
@@ -0,0 +1,25 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEditor;
+using UnityEngine;
+using Oni.Attributes;
+
+namespace Oni.Editor.Attributes
+{
+	/// <summary>
+	/// Drawer for drawing DisableEditing attribute
+	/// </summary>
+	[CustomPropertyDrawer(typeof(DisableEditingAttribute))]
+	public class DisableEditingDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			GUI.enabled = false;
+
+			EditorGUI.PropertyField(position, property, label);
+
+			GUI.enabled = true;
+		}
+	}
+}

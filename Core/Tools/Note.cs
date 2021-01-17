63fe5619a6bc5a6838b0ce3db5840f25102c2ea5
Unknown <nkmacadam@gmail.com>
2021-01-17 14:43:08 -0500

    Added Note component

---
+++ Core/Tools/Note.cs
@@ -0,0 +1,25 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+
+namespace Oni.Tools
+{
+	public class Note : MonoBehaviour
+	{
+#if UNITY_EDITOR
+		public enum NoteType
+		{
+			None,
+			Info,
+			Warning,
+			Error
+		}
+
+#pragma warning disable 414
+		[SerializeField] private string _message = default;
+		[SerializeField] private NoteType _type = default;
+#pragma warning restore 414
+#endif
+    }
+}
---
+++ Editor/Tools & Windows/NoteEditor.cs
@@ -0,0 +1,58 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+using UnityEditor;
+using System.Text.RegularExpressions;
+using System;
+using Oni.Tools;
+
+namespace Dream.Editor
+{
+	[CustomEditor(typeof(Note))]
+	public class NoteEditor : UnityEditor.Editor 
+	{
+		private SerializedProperty _message;
+		private SerializedProperty _type;
+
+		private GUIContent _infoIcon;
+		private GUIContent _warningIcon;
+		private GUIContent _errorIcon;
+
+		private void OnEnable()
+		{
+			_message = serializedObject.FindProperty("_message");
+			_type = serializedObject.FindProperty("_type");
+
+			_infoIcon = EditorGUIUtility.IconContent("console.infoicon.inactive.sml");
+			_warningIcon = EditorGUIUtility.IconContent("console.warnicon.inactive.sml");
+			_errorIcon = EditorGUIUtility.IconContent("console.erroricon.inactive.sml");
+		}
+
+		public override void OnInspectorGUI() 
+		{
+			EditorGUILayout.BeginVertical("box");
+
+			EditorGUILayout.BeginHorizontal();
+
+			DrawIcon();
+
+			EditorGUILayout.LabelField(_message.stringValue, EditorStyles.wordWrappedLabel);
+
+			EditorGUILayout.EndHorizontal();
+
+			EditorGUILayout.EndVertical();
+		}
+
+		private void DrawIcon()
+		{
+			switch ((Note.NoteType)_type.enumValueIndex)
+			{
+				case Note.NoteType.None: return;
+				case Note.NoteType.Info: GUILayout.Label(_infoIcon); return;
+				case Note.NoteType.Warning: GUILayout.Label(_warningIcon); return;
+				case Note.NoteType.Error: GUILayout.Label(_errorIcon); return;
+			}
+		}
+	}
+}

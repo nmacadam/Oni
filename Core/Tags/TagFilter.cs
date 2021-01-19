f18f889e65f5fbcaf5e044a07b8153cd633e5104
Unknown <nkmacadam@gmail.com>
2021-01-18 21:45:20 -0500

    Added TagFilter helper

---
+++ Core/Tags/TagFilter.cs
@@ -0,0 +1,31 @@
+// Dream Frontier, Copyright (c) DARUMA WORKS, All rights reserved.
+// Author: Nathan MacAdam
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oni
+{
+    [System.Serializable]
+    public class TagFilter
+	{
+		private enum CheckType
+		{
+			All,
+			Any
+		}
+
+		[SerializeField] private List<Tag> _checkForTags = new List<Tag>();
+		[SerializeField] private CheckType _require = default;
+
+		public bool Check(GameObject gameObject)
+		{
+			if (_checkForTags.Count == 0)
+			{
+				return true;
+			}
+
+            return gameObject.HasTags(_checkForTags.ToArray(), _require == CheckType.All);
+		}
+	}
+}
---
+++ Editor/Drawers/TagFilterDrawer.cs
@@ -0,0 +1,61 @@
+﻿// Dream Frontier, Copyright (c) DARUMA WORKS, All rights reserved.
+// Author: Nathan MacAdam
+
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Oni.Editor
+{
+	[CustomPropertyDrawer(typeof(TagFilter))]
+	public class TagFilterDrawer: PropertyDrawer 
+	{
+		private ReorderableList _list = null; 
+		
+		private SerializedProperty _requirments = null;
+
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		{ 
+			return -2.0f; 
+		}
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) 
+		{
+			if (_requirments == null)
+			{
+				_requirments = property.FindPropertyRelative("_require");
+			}
+
+			if (_list == null)
+			{
+				_list = new ReorderableList(property.serializedObject, property.FindPropertyRelative("_checkForTags"), true, true, true, true);
+				_list.drawElementCallback = DrawListItems;
+				_list.drawHeaderCallback = DrawHeader;
+			}
+
+			_list.DoLayoutList();
+		}
+
+		void DrawListItems(Rect rect, int index, bool isActive, bool isFocused)
+		{
+			SerializedProperty element = _list.serializedProperty.GetArrayElementAtIndex(index);
+			EditorGUI.PropertyField(
+				new Rect(rect.x, rect.y, rect.width, 2 + EditorGUIUtility.singleLineHeight),
+				element,
+				GUIContent.none
+			);
+		}
+
+		void DrawHeader(Rect rect)
+		{
+			float requirementWidth = 60f;
+			float requirementPercent = requirementWidth / rect.width;
+
+			var labelRect = new Rect(rect.x, rect.y, rect.width * (1 - requirementPercent), rect.height);
+			var requirementRect = new Rect((rect.width * (1 - requirementPercent)) + rect.x, rect.y, rect.width * requirementPercent, rect.height);
+
+			EditorGUI.LabelField(labelRect, "Tag Filter");
+			EditorGUI.PropertyField(requirementRect, _requirments, GUIContent.none);
+		}
+	}
+}

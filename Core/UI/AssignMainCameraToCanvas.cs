f96027ced9c651c10f75cdfe683325a0ef8b6af9
Unknown <nkmacadam@gmail.com>
2021-01-21 10:14:51 -0500

    Added AssignMainCameraToCanvas

---
+++ Core/UI/AssignMainCameraToCanvas.cs
@@ -0,0 +1,17 @@
+﻿// ONI, Copyright (c) Nathan MacAdam, All rights reserved. 
+// MIT License (See LICENSE file)
+
+using UnityEngine;
+
+namespace Oni.UI
+{
+	[RequireComponent(typeof(Canvas))]
+	public class AssignMainCameraToCanvas : MonoBehaviour
+	{
+		private void Awake() 
+		{
+			Canvas canvas = GetComponent<Canvas>();
+			canvas.worldCamera = Camera.main;
+		}
+	}
+}

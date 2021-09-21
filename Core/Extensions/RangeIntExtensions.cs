7715191601908c1666a513e61c4fd27a39960762
Nathan <nkmacadam@gmail.com>
2021-09-20 22:59:21 -0500

    Added Random extension methods to Range and RangeInt

---
+++ Core/Extensions/RangeIntExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Oni
+{
+    public static class RangeIntExtensions
+    {
+        /// <summary>
+        /// Returns a random value with this range's min (inclusive) and max (exclusive)
+        /// </summary>
+        /// <param name="range">This range</param>
+        /// <returns>A random integer with this range's min and max</returns>
+        public static float Random(this RangeInt range)
+        {
+            return UnityEngine.Random.Range(range.Min, range.Max);
+        }
+    }
+}
